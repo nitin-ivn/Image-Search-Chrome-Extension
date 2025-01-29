0b1f02f72ebc4ec7a88562fb8de3530c4395a8c4
nitin-ivn <ivnsainitin@gmail.com>
2025-01-29 16:24:05 +0530

    dataurl to blob

--- popup/popup.js
+++ popup/popup.js
@@ -45,3 +45,26 @@ document.getElementById("crop").addEventListener('click', () => {
     document.getElementById("crop").style.display = "none"
     searchOptions.style.display = "block";
 })
+
+
+document.getElementById("searchImage").addEventListener('click', () => {
+    let data = previewImg.src;
+    let blobOfData = dataURLtoBlob(data);
+    const objectURL = URL.createObjectURL(blobOfData);
+    console.log(objectURL);
+
+    const searchUrl2 = 'https://tineye.com/search?url=${encodeURIComponent(objectURL)}';
+
+    const searchUrl = 'https://www.google.com/searchbyimage?image_url=${encodeURIComponent(objectURL)}';
+    chrome.tabs.create({ url: searchUrl2 });
+})
+
+
+function dataURLtoBlob(dataurl) {
+    var arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    return new Blob([u8arr], {type:mime});
+}
